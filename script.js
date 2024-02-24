7af67d625892eabcf7603b985c10ad69756ce817
SLValerio <vsirlance@gmail.com>
2024-02-25 00:24:49 +0800

    improve ui/ux of login page + overall site icon

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-import { initializeApp } from
-    'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js';
-import { getDatabase, ref, push, set, get, update, remove } from
-    'https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js';
-
-
 // Firebase project configuration
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCeJlqfAu___gl2PxqVg9VNgjErlCmIxxQ",
     authDomain: "clover-planner.firebaseapp.com",
@@ -13,8 +10,8 @@ const firebaseConfig = {
     storageBucket: "clover-planner.appspot.com",
     messagingSenderId: "936646667254",
     appId: "1:936646667254:web:afd6229dd483c07140b5d7"
-  };
-  
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
@@ -25,10 +22,42 @@ export function checkPassword() {
     const correctPassword = "Clover0320";
     // Password correct, proceed to home page
     if (passwordInput === correctPassword) {
-      alert("Welcome, Lance!");
-      window.location.href = "./home/home.html"; 
+        showModal("Welcome, Lance!");
     } else {
-      // Password incorrect, show error message
-      alert("Password Incorrect, Try Again!");
+        // Password incorrect, show error message
+        showModal("Password Incorrect, Try Again!");
     }
-  }
+}
+
+/* JS of Modals */
+// Function to show modal
+function showModal(message) {
+    const modal = document.getElementById('passwordModal');
+    const modalOverlay = document.querySelector('.modal-overlay');
+    const modalMessage = document.getElementById('modalMessage');
+    const modalCloseBtn = document.getElementById('modalCloseBtn');
+    modalMessage.textContent = message;
+    modal.style.display = 'flex';
+    modalOverlay.style.zIndex = 1;
+    modalCloseBtn.style.zIndex = 1;
+
+    if (message === "Welcome, Lance!") {
+        modalCloseBtn.addEventListener('click', function() {
+            window.location.href = "./home/home.html"; 
+        });
+    }
+}
+
+// Function to close modal
+function closeModal() {
+    const modal = document.getElementById('passwordModal');
+    const modalOverlay = document.querySelector('.modal-overlay');
+    modal.style.display = 'none';
+    modalOverlay.style.zIndex = -1;
+}
+
+// Attach event listener to modal close button
+const modalCloseBtn = document.getElementById('modalCloseBtn');
+modalCloseBtn.addEventListener('click', closeModal);
+
+/* End of JS of Modals */
