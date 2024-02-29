72cfc41bb474cc6b1f4e84512cd5333bc85e2419
SLValerio <vsirlance@gmail.com>
2024-02-29 09:15:56 +0800

    fix eye icons

--- script.js
+++ script.js
@@ -23,12 +23,12 @@ export function togglePasswordVisibility() {
 
     if (passwordInput.type === 'password') {
         passwordInput.type = 'text';
-        eyeIcon.classList.remove('fa-eye');
-        eyeIcon.classList.add('fa-eye-slash');
-    } else {
-        passwordInput.type = 'password';
         eyeIcon.classList.remove('fa-eye-slash');
         eyeIcon.classList.add('fa-eye');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
     }
 }
 
